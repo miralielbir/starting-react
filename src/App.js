5805fa9024db729c56258d871dc0b89777ce6bec
miralielbir <miralielbir@gmail.com>
2025-09-12 22:47:44 +0300

    oley

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div  style ={{
+      margin: "auto",
+      width: 800,
+      paddingTop: "1rem"
+    }}>
+      <h1 className="title">Best Hamburger Search</h1>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>BuRger Maniac</td>
+            <td>150£ Full Onion</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
