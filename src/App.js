edbbe9b3aa8f62277b683487ac2d05961a0d677c
miralielbir <miralielbir@gmail.com>
2025-09-17 15:22:54 +0300

    brgrr

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
 import React from "react";
 import './App.css';
 import PropTypes from "prop-types";
-import burger from "./burger.json"
+import styled from '@emotion/styled';
+import Button from '@mui/material/Button';
+import { selectClasses } from "@mui/material";
 
 const BurgerRow =({ burger, onSelect})=>( 
           <tr>
             <td>{burger.name}</td>
             <td>{burger.description}</td>
             <td>
-              <button
-              onClick={()=>onSelect(burger)}>Select!</button>
+              <Button variant="contained" color="primary"
+              onClick={()=>onSelect(burger)}>Select!</Button>
             </td>
           </tr>
 
@@ -41,28 +42,54 @@ const BurgerInfo =({name, patty_type,toppings
 
   </div>
 
-)
+);
 
-
-function App() {
-  const [filter,filterSet] = React.useState("");
-  const [selectedItem, selectedItemSet]=React.useState(null);
-  return (
-    <div  style ={{
-      margin: "auto",
-      width: 800,
-      paddingTop: "1rem"
-    }}>
-      <h1 className="title">Best Hamburger Search</h1>
-      <input
-      value={filter}
-      onChange={(evt)=>filterSet(evt.target.value)}
+const Title = styled.h1`
+  text-align:center;
+`;
+const TwoColumnLayout=styled.div`
+        display:grid;
+        grid-template-columns: 70% 30%;
+        grid-column-gap:1rem;
+`;
+const Yo= styled.div`
+      margin: auto;
+      width: 800px;
+      padding-top: 1rem;
+`;
+const Input = styled.input`
+  width:100%;
+  font-size:x-large;
+  padding:0.2rem;
+`;
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      filter:"",
+      burger:[],
+      selectedItem:null,
+    };
+  }
+  componentDidMount(){
+    fetch("http://localhost:3000/starting-react/burger.json")
+    .then(resp =>resp.json())
+    .then(burger =>this.setState({
+      ...this.state,
+      burger,
+  }));
+  
+  }
+  render(){
+ return (
+    <Yo>
+      <Title>Best Hamburger Search</Title>
+      <Input
+      value={this.statefilter}
+      onChange={(evt)=>this.setState({
+        ...this.state,fiter:evt.target.value})}
       />
-      <div style={{
-        display:"grid",
-        gridTemplateColumns: "70% 30%",
-        gridColumnGap:"1rem",
-      }}>
+      <TwoColumnLayout>
       <div>
       <table width="100%">
         <thead>
@@ -72,19 +99,30 @@ function App() {
           </tr>
         </thead>
         <tbody>
-                  {burger.filter((burger)=> burger.name.toLowerCase().includes(filter.toLowerCase()))
+                  {this.state.burger.filter((burger)=> burger.name.toLowerCase().includes(this.state.filter.toLowerCase()))
                   .map((burger) => (
-                    <BurgerRow burger={burger} key={[burger.name, burger.patty_type].join(":")} onSelect={(burger)=>selectedItemSet(burger)}/>
+                    <BurgerRow burger={burger} key={[burger.name, burger.patty_type].join(":")} onSelect={(burger)=>this.setState({
+                      ...this.state,selectedItem:burger})}/>
 
         ))}
         </tbody>
         </table>
         </div>
-        {selectedItem &&<BurgerInfo {...selectedItem}/>
+        {this.state.selectedItem &&<BurgerInfo {...this.state.selectedItem}/>
         }
-        </div>
-      </div>
+        </TwoColumnLayout>
+      </Yo>
   );
+  }
 }
+
+
+/*
+  React.useEffect(()=>{
+    fetch("http://localhost:3000/starting-react/burger.json")
+    .then(resp =>resp.json())
+    .then(data =>burgerSet(data));
+  }, []);
+  */
 
 export default App;
